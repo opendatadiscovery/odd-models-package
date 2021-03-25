8e6eaa6d29d8bb7f6837de3d2c146a92a193c0ad
Nikita Dementev <ndementev@provectus.com>
2021-03-25 15:16:09 +0300

    Fix controller injection design error

--- odd_contract/__init__.py
+++ odd_contract/__init__.py
@@ -5,14 +5,16 @@ from .encoder import JSONEncoder
 from odd_contract.controllers import ODDController, ControllerHolder
 
 
-def init_flask_app(controller: ODDController):
+def init_flask_app():
     app = connexion.App(__name__, specification_dir='openapi')
     app.add_api(specification='openapi.yaml',
                 arguments={'title': 'ODD adapter HTTP API contract'},
                 pythonic_params=True)
 
-    ControllerHolder.init_controller(controller)
-
     app = app.app
     app.json_encoder = JSONEncoder
     return app
+
+
+def init_controller(controller: ODDController):
+    ControllerHolder.init_controller(controller)
