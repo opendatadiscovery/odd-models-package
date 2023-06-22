8cc1be0e7b0415dc515a960824ba8a3688f956cb
Pavel Makarichev <vixtir90@gmail.com>
2023-06-22 18:34:09 +0300

    docs: update postgres example

--- examples/client.py
+++ examples/client.py
@@ -6,10 +6,10 @@ from examples.postgres_models import data_entity_list, generator
 from odd_models.api_client.v2.odd_api_client import Client
 
 client = Client(host="http://localhost:8080")
-client.auth(name="dev_aws_token", description="Token for dev AWS account data sources")
+client.auth(name="postgres", description="Token for dev AWS account data sources")
 
 client.create_data_source(
     data_source_oddrn=generator.get_data_source_oddrn(),
-    data_source_name="AWS dev account data source",
+    data_source_name="Postgres data source",
 )
-client.ingest_data_entity_list(data_entity_list)
+client.ingest_data_entity_list(data_entities=data_entity_list)
